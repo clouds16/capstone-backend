c355b7116aacaeb659a07753c8fc5f6aaec97453
Hector Alvarez-Toledo <hect16@gmail.com>
2021-12-01 18:21:19 -0600

    bug fix to a dependency

--- models/stats.js
+++ models/stats.js
@@ -1,17 +1,16 @@
 const User =  require('./users')
 const mongoose = require('mongoose');
-const validator = require('validate')
+const validator = require('validator')
 
 const Stats = mongoose.model('Stats', new mongoose.Schema({
     weight: {
         type: Number,
         required: true,
         validate(value) {
-            if (value < 50) {
-                throw new Error("Weight is too low")
+            if (value < 50 ) {
+                throw new Error("Weight cannot be that low")
             }
         }
-        
     },
     units : {
         required :true, 
