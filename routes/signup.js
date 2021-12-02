1f5e6745570a6ca5872eae8154f9416bfd2a5776
Hector Alvarez-Toledo <hect16@gmail.com>
2021-12-02 15:01:14 -0600

    vonage texts

--- routes/signup.js
+++ routes/signup.js
@@ -5,27 +5,43 @@ const nodemailer = require("nodemailer");
 
 const mailjet = require ('node-mailjet');
 mailjet.connect('34e259d28c17b5aae05273fa38e81879', '7571d6748fb3a63db2c6425d8454d709')
+const Vonage = require('@vonage/server-sdk')
+
+const vonage = new Vonage({
+  apiKey: "48eb7b52",
+  apiSecret: "BogSTZQcjII91mOQ"
+})
+
+
 
 router.post('/signup', async function(req, res ) {
 	const accountSid = process.env.TWILIO_ACCOUNTSID; 
 	const authToken = process.env.TWILIO_APIKEY; 
 	const client = require('twilio')(accountSid, authToken); 
-	
+
+
+	const from = "18664851562"
+	const to = req.body.phone	
+	const text = 'Thank you for signing up to Motivatr!'
+
+
 
 	const user = User(req.body);
 	console.log(req.body)
 	try {
 	  await user.save() ;
-	//   client.messages 
-    //   .create({ 
-    //      body: 'Thank you '+ req.body.fname + ' ,' + req.body.lname  + ' for signing up for Motivatr!',  
-    //      messagingServiceSid: 'MG51d38ea974422fa5e7735f65133f87c6',      
-    //      to: req.body.phone
-    //    }) 
-    //   .then(message => console.log(message.sid)) 
-    //   .done();
-
-	  //send back status
+	  
+	  vonage.message.sendSms(from, to, text, (err, responseData) => {
+		if (err) {
+			console.log(err);
+		} else {
+			if(responseData.messages[0]['status'] === "0") {
+				console.log("Message sent successfully.");
+			} else {
+				console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+			}
+		}
+	})
 	  res.status(200).send(user)
 
 	} catch (e) {
