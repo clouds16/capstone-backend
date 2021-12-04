1a0bd1b7e91ebf7bc776cbed8ca773e578a48751
Hector Alvarez-Toledo <hect16@gmail.com>
2021-12-03 20:16:46 -0600

    some changes

--- routes/addworkout.js
+++ routes/addworkout.js
@@ -4,17 +4,23 @@ const Workouts = require("../models/workouts")
 
 router.post("/profile/:id/addworkout" , async  (req, res) => {
 	console.log(req.body)
-	let workout = new Workouts(req.body)
-	
-	try {
-		await workout.save()
-		console.log("success", workout)
-		res.send(workout)
 
-	} catch (e) {
-		console.log("error")
-		res.sendStatus(300).send(e) 
+	if (req.body.sets != null && req.body.force != null) {
+		let workout = new Workouts(req.body)
+		
+		try {
+			await workout.save()
+			console.log("success", workout)
+			res.send(workout)
+	
+		} catch (e) {
+			console.log("error")
+			res.sendStatus(300)
+		}
 	}
+	
+	
+	
 })
 
 
